d0e901680fb9a9eea4d58d22c44be0872c97fe75
Iain Millar <iain.millar@live.co.uk>
2024-01-14 20:22:40 +0000

    Update ValidationException.java

--- src/main/java/com/im/scratchpad/ValidationException.java
+++ src/main/java/com/im/scratchpad/ValidationException.java
@@ -1,7 +1,7 @@
 package com.im.scratchpad;
 
 /**
- * Simple exception wrapper to highlight an validation
+ * Simple exception wrapper to highlight a validation
  * exception use case.
  */
 public class ValidationException extends Exception {
